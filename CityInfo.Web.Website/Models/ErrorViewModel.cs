974297b08398ef473ff7e67940e2f73f094380c8
yogichandras <yogichandras@WIN-D9T1Q3KRDLF>
2018-04-18 19:04:06 +0700

    add landing page

---
+++ CityInfo.Web.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CityInfo.Web.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
